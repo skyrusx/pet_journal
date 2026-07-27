ce5e23ffa165dc3d81136f5d2bc615813d0a2eeb
Ruslan Fedotov <progruson@gmail.com>
2026-07-28 00:34:28 +0700

    Replace Devise routes with clean URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
   root "pages#index"
-  devise_for :users
+
+  devise_for :users, skip: %i[sessions registrations passwords]
+  devise_scope :user do
+    get "login" => "devise/sessions#new", as: :new_user_session
+    post "login" => "devise/sessions#create", as: :user_session
+    delete "logout" => "devise/sessions#destroy", as: :destroy_user_session
+
+    get "register" => "devise/registrations#new", as: :new_user_registration
+    post "register" => "devise/registrations#create", as: :user_registration
+    get "account" => "devise/registrations#edit", as: :edit_user_registration
+    patch "account" => "devise/registrations#update"
+    put "account" => "devise/registrations#update"
+    delete "account" => "devise/registrations#destroy"
+    get "account/cancel" => "devise/registrations#cancel", as: :cancel_user_registration
+
+    get "password/new" => "devise/passwords#new", as: :new_user_password
+    get "password/edit" => "devise/passwords#edit", as: :edit_user_password
+    post "password" => "devise/passwords#create", as: :user_password
+    patch "password" => "devise/passwords#update"
+    put "password" => "devise/passwords#update"
+  end
+
   resources :notification_channels, except: %i[show] do
     post :test, on: :member
     post "deliveries/:delivery_id/retry", action: :retry_delivery, on: :collection, as: :retry_delivery
