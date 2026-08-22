8cefab4907a158569d5a51a3a56430556fafeb17
Ruslan Fedotov <progruson@gmail.com>
2026-08-22 15:55:41 +0700

    Localize settings time zones

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,12 +3,26 @@ class SettingsController < ApplicationController
 
   before_action :authenticate_user!
 
+  RUSSIAN_TIME_ZONES = {
+    "Kaliningrad" => "Калининград",
+    "Moscow" => "Москва",
+    "Samara" => "Самара",
+    "Ekaterinburg" => "Екатеринбург",
+    "Omsk" => "Омск",
+    "Krasnoyarsk" => "Красноярск",
+    "Irkutsk" => "Иркутск",
+    "Yakutsk" => "Якутск",
+    "Vladivostok" => "Владивосток",
+    "Magadan" => "Магадан",
+    "Kamchatka" => "Камчатка"
+  }.freeze
+
   def edit
-    @time_zones = ActiveSupport::TimeZone.all
+    prepare_time_zone_options
   end
 
   def update
-    @time_zones = ActiveSupport::TimeZone.all
+    prepare_time_zone_options
 
     if current_user.update(settings_params)
       redirect_to settings_path, notice: "Настройки сохранены."
@@ -21,5 +35,16 @@ class SettingsController < ApplicationController
 
   def settings_params
     params.require(:user).permit(:interface_text_size, :notifications_time_zone)
+  end
+
+  def prepare_time_zone_options
+    @time_zone_options = [["(UTC+00:00) Всемирное координированное время", "UTC"]]
+
+    RUSSIAN_TIME_ZONES.each do |zone_name, russian_name|
+      zone = ActiveSupport::TimeZone[zone_name]
+      next unless zone
+
+      @time_zone_options << ["(UTC#{zone.formatted_offset}) #{russian_name}", zone.name]
+    end
   end
 end
