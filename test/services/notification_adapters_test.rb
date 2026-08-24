4bf680249c5b77df0cf3bf36f87ebdf950fc17b7
Ruslan Fedotov <progruson@gmail.com>
2026-08-24 21:02:15 +0700

    Test VK delivery through centralized configuration

--- test/services/notification_adapters_test.rb
+++ test/services/notification_adapters_test.rb
@@ -45,4 +45,34 @@ class NotificationAdaptersTest < ActiveSupport::TestCase
     assert_equal Rails.application.routes.url_helpers.reminders_overview_path(pet_id: reminder.pet_id), payload["path"]
     assert_equal "reminder-#{reminder.id}", payload["tag"]
   end
+
+  test "VK adapter uses centralized credentials configuration" do
+    user = users(:one)
+    reminder = reminders(:one)
+    channel = user.notification_channels.create!(
+      channel_type: :vk,
+      name: "VK",
+      address: "123456",
+      enabled: true
+    )
+    delivery = reminder.notification_deliveries.create!(notification_channel: channel)
+    submitted = nil
+    response = Struct.new(:body).new({ response: 1 }.to_json)
+
+    VkConfiguration.stub(:group_token, "credential-token") do
+      VkConfiguration.stub(:api_version, "5.199") do
+        Net::HTTP.stub(:post_form, ->(uri, params) { submitted = [uri, params]; response }) do
+          NotificationAdapters.for(channel).deliver(delivery)
+        end
+      end
+    end
+
+    uri, params = submitted
+    assert_equal "https://api.vk.com/method/messages.send", uri.to_s
+    assert_equal "credential-token", params[:access_token]
+    assert_equal "123456", params[:peer_id]
+    assert_equal "5.199", params[:v]
+    assert_includes params[:message], reminder.pet.name
+    assert_includes params[:message], reminder.title
+  end
 end
