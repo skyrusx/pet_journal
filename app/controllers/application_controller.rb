8c7caba621651f09e7a160af47be6d3c9a48d9c9
Ruslan Fedotov <progruson@gmail.com>
2026-08-24 22:43:31 +0700

    Use each user's notification time zone in signed-in requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  around_action :use_user_time_zone, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name phone avatar remove_avatar])
+  end
+
+  private
+
+  def use_user_time_zone(&block)
+    Time.use_zone(current_user.notifications_time_zone_name, &block)
   end
 end
