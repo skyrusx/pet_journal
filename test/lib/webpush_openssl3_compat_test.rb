06e7b49f2707a07888d5aab30f548249895a3605
Ruslan Fedotov <progruson@gmail.com>
2026-08-24 20:25:18 +0700

    Cover Web Push encryption on OpenSSL 3

--- test/lib/webpush_openssl3_compat_test.rb
+++ test/lib/webpush_openssl3_compat_test.rb
@@ -12,4 +12,15 @@ class WebpushOpenssl3CompatTest < ActiveSupport::TestCase
     assert_equal key.public_key, rebuilt.public_key
     assert_equal key.private_key, rebuilt.private_key
   end
+
+  test "encrypts a push payload without mutating an EC key" do
+    client_key = OpenSSL::PKey::EC.generate("prime256v1")
+    p256dh = Webpush.encode64(client_key.public_key.to_bn.to_s(2))
+    auth = Webpush.encode64(SecureRandom.random_bytes(16))
+
+    encrypted = Webpush::Encryption.encrypt("PetJournal test", p256dh, auth)
+
+    assert encrypted.present?
+    assert_operator encrypted.bytesize, :>, "PetJournal test".bytesize
+  end
 end
