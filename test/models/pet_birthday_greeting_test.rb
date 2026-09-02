3314b779ca752ad947fae854c5b70e239a967d68
Ruslan Fedotov <progruson@gmail.com>
2026-09-02 17:59:01 +0700

    Test birthday greeting cleanup on account deletion

--- test/models/pet_birthday_greeting_test.rb
+++ test/models/pet_birthday_greeting_test.rb
@@ -8,4 +8,13 @@ class PetBirthdayGreetingTest < ActiveSupport::TestCase
     assert greeting.reload.shown_at.present?
     assert_not greeting.claim_for_display!
   end
+
+  test "birthday greetings are removed with user" do
+    user = users(:one)
+    PetBirthdayGreeting.create!(user: user, greeting_date: Date.new(2026, 9, 2))
+
+    assert_difference "PetBirthdayGreeting.count", -1 do
+      user.destroy!
+    end
+  end
 end
