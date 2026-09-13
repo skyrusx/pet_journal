c3c119ea38e21a76524aea962c2174e0867e84ae
Ruslan Fedotov <progruson@gmail.com>
2026-09-13 15:06:31 +0700

    Format user last activity in admin

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -37,4 +37,20 @@ module Admin::ApplicationHelper
   def admin_role_label(user)
     user.admin? ? "Администратор" : "Пользователь"
   end
+
+  def admin_last_seen_label(user)
+    return "Нет данных" if user.last_seen_at.blank?
+
+    time = user.last_seen_at.in_time_zone(current_user.notifications_time_zone_name)
+    today = Time.current.in_time_zone(current_user.notifications_time_zone_name).to_date
+
+    case time.to_date
+    when today
+      "Сегодня, #{time.strftime('%H:%M')}"
+    when today - 1.day
+      "Вчера, #{time.strftime('%H:%M')}"
+    else
+      time.strftime("%d.%m.%Y %H:%M")
+    end
+  end
 end
