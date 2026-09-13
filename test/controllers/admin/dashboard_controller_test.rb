8175154501dd8f82dab648d195560008fe21ee29
Ruslan Fedotov <progruson@gmail.com>
2026-09-13 17:05:32 +0700

    Cover admin mobile chrome

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -25,6 +25,20 @@ class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", "Обзор"
   end
 
+  test "renders mobile admin chrome" do
+    sign_in users(:admin)
+
+    get admin_root_path
+
+    assert_response :success
+    assert_select "header.admin-mobile-chrome__header"
+    assert_select "nav.admin-mobile-tabs"
+    assert_select "nav.admin-mobile-tabs a", text: /Обзор/
+    assert_select "nav.admin-mobile-tabs a", text: /Пользователи/
+    assert_select "nav.admin-mobile-tabs a", text: /Питомцы/
+    assert_select "nav.admin-mobile-tabs a", text: /PetJournal/
+  end
+
   test "links dashboard metrics to matching admin catalogs" do
     sign_in users(:admin)
 
